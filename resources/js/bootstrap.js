46bfea7845d3b08b7d8c136c779ba7d86b00ce81
Bubka <858858+Bubka@users.noreply.github.com>
2019-06-24 00:26:04 +0200

    bulma & font-awesome in place of Bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+    //require('bootstrap');
 } catch (e) {}
 
 /**
