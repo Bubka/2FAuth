dc64aa55ec98518774630727786380fce19f0f3c
Bubka <858858+Bubka@users.noreply.github.com>
2019-06-24 00:22:37 +0200

    new 2FAccounts seeder

---
+++ database/seeds/TwoFAccountsTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+use App\TwoFAccount;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\WithFaker;
+class TwoFAccountsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        TwoFAccount::create([
+            'name' => $faker->unique()->domainName,
+            'email' => $faker->email,
+            'uri' => 'otpauth://totp/test@test.com?secret=A4GRFTVVRBGY7UIW&issuer=test',
+            'icon' => 'https://fakeimg.pl/64x64/'
+        ]);
+        TwoFAccount::create([
+            'name' => $faker->unique()->domainName,
+            'email' => $faker->email,
+            'uri' => 'otpauth://totp/test@test.com?secret=A4GRFHYVRBGY7UIW&issuer=test',
+            'icon' => 'https://fakeimg.pl/64x64/'
+        ]);
+        TwoFAccount::create([
+            'name' => $faker->unique()->domainName,
+            'email' => $faker->email,
+            'uri' => 'otpauth://totp/test@test.com?secret=A4GRFHZVRBGY7UIW&issuer=test',
+            'icon' => 'https://fakeimg.pl/64x64/'
+        ]);
+        TwoFAccount::create([
+            'name' => $faker->unique()->domainName,
+            'email' => $faker->email,
+            'uri' => 'otpauth://totp/test@test.com?secret=A4GRFHVIRBGY7UIW&issuer=test',
+            'icon' => 'https://fakeimg.pl/64x64/'
+        ]);
+        TwoFAccount::create([
+            'name' => $faker->unique()->domainName,
+            'email' => $faker->email,
+            'uri' => 'otpauth://totp/test@test.com?secret=A4GRFHVVOBGY7UIW&issuer=test',
+            'icon' => 'https://fakeimg.pl/64x64/'
+        ]);
+    }
+}
