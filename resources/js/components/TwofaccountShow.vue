a5858093b0d1eef8f32eabf26a81ef28592a95ef
Bubka <858858+Bubka@users.noreply.github.com>
2020-01-02 00:07:47 +0100

    Move edit link

--- resources/js/components/TwofaccountShow.vue
+++ resources/js/components/TwofaccountShow.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        <router-link :to="{ name: 'edit', params: { twofaccountId: twofaccountid }}">Edit</router-link>
-        <router-link :to="{ name: 'delete', params: { twofaccountId: twofaccountid }}">Delete</router-link>
         <figure class="image is-64x64" style="display: inline-block">
             <img :src="icon">
         </figure>
         <p class="is-size-4 has-text-grey-light">{{ name }}</p>
         <p class="is-size-6 has-text-grey">{{ email }}</p>
         <slot></slot>
+        <div class="modal-action-links has-text-grey">
+            <router-link :to="{ name: 'edit', params: { twofaccountId: twofaccountid }}">Edit</router-link>
+        </div>
     </div>
 </template>
 
