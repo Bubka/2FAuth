14e486094afdc396c7c2df37cdde979af34add3b
Bubka <858858+Bubka@users.noreply.github.com>
2020-01-12 15:22:20 +0100

    fix missing i18n generated file in mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/bootstrap.js', 'public/js')
+    .js('resources/js/vue-i18n-locales.generated.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
