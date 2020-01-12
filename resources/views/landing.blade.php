e2a49e19ccf9915415bd3d16cb17457eff4cf1e3
Bubka <858858+Bubka@users.noreply.github.com>
2020-01-12 20:12:25 +0100

    Complete html header for webapp capability

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no">
     <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <title>{{ env("APP_NAME") }}</title>
     <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
 </head>
