3cf4d964e75cbd2dd0d56175bd6b0bcd8922426c
Bubka <858858+Bubka@users.noreply.github.com>
2020-01-14 23:43:43 +0100

    Update locales

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -31,7 +31,10 @@ return [
         'password_do_not_match' => 'Password do not match',
         'forgot_your_password' => 'Forgot your password?',
         'request_password_reset' => 'Request a password reset',
-    ]
+    ],
+    'send_password_reset_link' => 'Send password reset link',
+    'reset_password' => 'Reset your password',
+    'new_password' => 'New password',
 
 
 ];
