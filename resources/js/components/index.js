9926725a04aea2e6346d1648d5d87066c476e3a7
Bubka <858858+Bubka@users.noreply.github.com>
2020-01-19 21:06:57 +0100

    Remove Form component

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import Button from './Button'
-import { FormError, FormErrors, FieldError } from './form'
 
 // Components that are registered globaly.
 [
 	Button,
-    FormError,
-    FormErrors,
-    FieldError
 ].forEach(Component => {
 	Vue.component(Component.name, Component)
 })
