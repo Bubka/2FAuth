9c3b77e56a46cbe967df28fe9eb2a08c89aef0ed
Bubka <858858+Bubka@users.noreply.github.com>
2020-02-14 11:42:39 +0100

    Set language  via the html lang attribute

--- resources/js/langs/i18n.js
+++ resources/js/langs/i18n.js
@@ -4,8 +4,7 @@ import Locale from './locales';
 
 Vue.use(VueInternationalization);
 
-// const lang = document.documentElement.lang.substr(0, 2);
-const lang = 'en';
+const lang = document.documentElement.lang.substr(0, 2);
 
 const i18n = new VueInternationalization({
     locale: lang,
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="has-background-black-ter">
+<html class="has-background-black-ter" lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
