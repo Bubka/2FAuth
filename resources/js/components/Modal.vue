eafff6c17ef9c03c434cac40baf2fee38a92ceb4
Bubka <858858+Bubka@users.noreply.github.com>
2020-09-25 13:46:07 +0200

    Use a more mobile friendly Close button for modal

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="modal modal-otp" v-bind:class="{ 'is-active': isActive }">
-      <div class="modal-background" @click.stop="closeModal"></div>
-      <div class="modal-content">
-        <section class="section">
-            <div class="columns is-centered">
-                <div class="column is-three-quarters">
-                    <div class="box has-text-centered has-background-black-ter is-shadowless">
-                        <slot></slot>
+        <div class="modal-background" @click.stop="closeModal"></div>
+        <div class="modal-content">
+            <section class="section">
+                <div class="columns is-centered">
+                    <div class="column is-three-quarters">
+                        <div class="box has-text-centered has-background-black-ter is-shadowless">
+                            <slot></slot>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-      </div>
-      <button class="modal-close is-large" aria-label="close" @click.stop="closeModal"></button>
+            </section>
+        </div>
+        <div class="fullscreen-footer">
+            <!-- Close button -->
+            <label class="button is-dark is-rounded" @click.stop="closeModal">
+                {{ $t('commons.close') }}
+            </label>
+        </div>
     </div>
 </template>
 
