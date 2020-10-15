ae770fe2f0aea9815b3a783fefb61f5f4fa506bf
Bubka <858858+Bubka@users.noreply.github.com>
2020-10-15 13:57:07 +0200

    Update 'en' lang with Laravel 7 terms & translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -12,12 +12,16 @@ return [
     | these language lines according to your application's requirements.
     |
     */
+   
+    // Laravel
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    // 2FAuth
     'sign_out' => 'Sign out',
     'sign_in' => 'Sign in',
     'register' => 'Register',
     'hello' => 'Hi {username} !',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'already_authenticated' => 'Already authenticated',
     'confirm' => [
         'logout' => 'Are you sure you want to log out?',
