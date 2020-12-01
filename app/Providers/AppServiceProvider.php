e7156d0650caaf496e5ce583c1866dda2478590c
Bubka <858858+Bubka@users.noreply.github.com>
2020-12-01 09:19:58 +0100

    Fix too long key error during migration for some MySQL setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::withoutComponentTags();
+        Schema::defaultStringLength(191);
     }
 }
