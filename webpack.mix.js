5dca0079a72a88af3608ddb8c731fbd2d7634d45
Bubka <858858+Bubka@users.noreply.github.com>
2021-11-11 16:12:51 +0100

    Add sourceMap generation (deactivated, for later use)

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+//mix.sourceMaps(false, "source-map")
+mix
+    .js('resources/js/app.js', 'public/js')
     .extract([
       'vue',
       'axios',
