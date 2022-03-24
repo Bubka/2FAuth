dbb17b3f12ffe986b878a079c9305f58a46d2e79
Bubka <858858+Bubka@users.noreply.github.com>
2022-03-24 15:08:46 +0100

    Remove useless routeMiddlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,16 +69,9 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'guest' => \App\Http\Middleware\RejectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'AvoidResetPassword' => \App\Http\Middleware\AvoidPasswordResetInDemo::class,
-        'Json.response' => \App\Http\Middleware\ForceJsonResponse::class,
     ];
 
     /**
