c2edd69c5479874170540327628c47339c80d5f4
Bubka <858858+Bubka@users.noreply.github.com>
2022-05-18 22:50:03 +0200

    Fix missing email address of remote user

--- app/Extensions/RemoteUserProvider.php
+++ app/Extensions/RemoteUserProvider.php
@@ -29,7 +29,7 @@ class RemoteUserProvider implements UserProvider
 
         $user = new User;
         $user->name = $identifier['user'];
-        $user->email = Arr::has($identifier, 'email') ? : 'user@auth.proxy';
+        $user->email = Arr::has($identifier, 'email') ? $identifier['email'] : 'fake.email@do.not.use';
 
         return $user;
     }
