f2b7664a2f80ff65caddd65e697175e2f78372b2
Bubka <858858+Bubka@users.noreply.github.com>
2022-06-01 00:10:29 +0200

    Fix logging of deleted ids

--- app/Services/TwoFAccountService.php
+++ app/Services/TwoFAccountService.php
@@ -221,7 +221,7 @@ class TwoFAccountService
         // $ids as string could be a comma-separated list of ids
         // so in this case we explode the string to an array
         $ids = $this->commaSeparatedToArray($ids);
-        Log::info(sprintf('Deletion of TwoFAccounts #%s requested', implode(',#', $ids)));
+        Log::info(sprintf('Deletion of TwoFAccounts #%s requested', is_array($ids) ? implode(',#', $ids) : $ids ));
         $deleted = TwoFAccount::destroy($ids);
 
         return $deleted;
