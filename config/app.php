b408f1984c352757c9fc0db4d9adbd9d560c6733
Bubka <858858+Bubka@users.noreply.github.com>
2022-07-21 15:44:04 +0200

    Fix provider group

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\TwoFAuthServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -175,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TwoFAuthServiceProvider::class,
 
     ],
 
