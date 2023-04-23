cb43b38ea1d6ba8eee73179313708497dc4ed444
theDepart3d <109445905+theDepart3d@users.noreply.github.com>
2023-04-23 18:08:41 +0200

    Readded DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Ability to seed data for testing purposes
+        //$this->call([
+        //    DemoSeeder::class,
+        //    TestingSeeder::class
+        //]);
+    }
+}
