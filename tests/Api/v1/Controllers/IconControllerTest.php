68d45be102eca3c6de367bcabe34a8ff3a306039
Bubka <858858+Bubka@users.noreply.github.com>
2023-09-13 10:04:44 +0200

    Fix test no returning the Twitter icon (thanks Elon...)

--- tests/Api/v1/Controllers/IconControllerTest.php
+++ tests/Api/v1/Controllers/IconControllerTest.php
@@ -63,7 +63,7 @@ class IconControllerTest extends FeatureTestCase
     {
         $response = $this->actingAs($this->user, 'api-guard')
             ->json('POST', '/api/v1/icons/default', [
-                'service' => 'twitter',
+                'service' => 'abode',
             ])
             ->assertStatus(201)
             ->assertJsonStructure([
