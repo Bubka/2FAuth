e393ca1ea0c2db8f58c0598d9082417c35dc8940
Bubka <858858+Bubka@users.noreply.github.com>
2023-09-21 17:10:12 +0200

    Set up the notification feature

--- resources/js_vue3/App.vue
+++ resources/js_vue3/App.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+    <notifications id="vueNotification" role="alert" width="100%" position="top" :duration="4000" :speed="0" :max="1" classes="notification is-radiusless" />
     <main class="main-section">
         <RouterView />
     </main>
--- resources/js_vue3/app.js
+++ resources/js_vue3/app.js
@@ -5,10 +5,12 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import Notifications from '@kyvg/vue3-notification'
 
 const app = createApp(App)
 
 // app.use(createPinia())
 app.use(router)
+app.use(Notifications)
 
 app.mount('#app')
