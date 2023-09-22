75cbb0e1ab5cfcefeb288501d3123f499cdf63c9
Bubka <858858+Bubka@users.noreply.github.com>
2023-09-22 11:32:24 +0200

    Set up global immutable app properties

--- resources/js_vue3/app.js
+++ resources/js_vue3/app.js
@@ -9,6 +9,15 @@ import Notifications from '@kyvg/vue3-notification'
 
 const app = createApp(App)
 
+// Immutable app properties provided by the laravel blade view
+app.config.globalProperties.$2fauth = {
+    config: window.appConfig,
+    version: window.appVersion,
+    isDemoApp: window.isDemoApp,
+    isTestingApp: window.isTestingApp,
+    langs: window.appLocales
+}
+
 // app.use(createPinia())
 app.use(router)
 app.use(Notifications)
