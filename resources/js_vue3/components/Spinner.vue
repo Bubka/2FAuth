8736941aa1e0d14a680f9534858813dc9ac23bd7
Bubka <858858+Bubka@users.noreply.github.com>
2023-10-05 14:03:00 +0200

    Set up a Spinner component

---
+++ resources/js_vue3/components/Spinner.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const props = defineProps({
+        isVisible: Boolean,
+        type: {
+            type: String,
+            default: 'inline'
+        },
+    })
+</script>
+
+<template>
+    <div v-if="isVisible">
+        <div v-if="type == 'inline'" class="has-text-centered mt-6">
+            <span id="icnSpinner" class="is-size-4">
+                <FontAwesomeIcon :icon="['fas', 'spinner']" spin />
+            </span>
+        </div>
+    </div>
+</template>
