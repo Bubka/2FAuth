8a102e32f655130502fa2160d941cc29cb2cd5a4
Bubka <858858+Bubka@users.noreply.github.com>
2023-12-05 18:47:34 +0100

    Restore DatabaseSeeder.php to run 'artisan migrate --seed' without error

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //$this->call([
+        //    DemoSeeder::class,
+        //    TestingSeeder::class
+        //]);
+    }
+}
