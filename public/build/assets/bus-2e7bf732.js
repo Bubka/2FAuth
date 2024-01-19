35f2f1df84b65e83aef6c26839452250f2df8c4b
Bubka <858858+Bubka@users.noreply.github.com>
2024-01-19 15:49:01 +0100

    Rebuild assets

---
+++ public/build/assets/bus-2e7bf732.js
@@ -0,0 +1,1 @@
+import{a0 as e}from"./app-347ef106.js";/*! 2FAuth version 5.0.3 - Copyright (c) 2023 Bubka - https://github.com/Bubka/2FAuth */const n=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null}),actions:{}});export{n as u};
