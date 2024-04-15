4e22fe7e2cc02687c2a4a67aee574ea473dd35ba
Bubka <858858+Bubka@users.noreply.github.com>
2024-04-15 21:42:04 +0200

    Add a silenced email channel for the Demo mode

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,11 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        
+        'silent' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
+        ],
     ],
 
 ];
