763a653e54c17d1732e96663844be7deb4fedec2
Bubka <858858+Bubka@users.noreply.github.com>
2024-09-25 08:42:45 +0200

    Remove deprecated apple specific meta tag

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no, viewport-fit=cover">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <meta name="robots" content="noindex, nofollow">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <title>{{ config('app.name') }}</title>
 
