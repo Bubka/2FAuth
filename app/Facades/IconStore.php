02fbd4c2854849c0c9df3127b1c550e13dffeb69
Bubka <858858+Bubka@users.noreply.github.com>
2024-11-09 14:44:24 +0100

    Complete phpdocs of facades with non-static methods

--- app/Facades/IconStore.php
+++ app/Facades/IconStore.php
@@ -6,6 +6,16 @@ use App\Services\IconStoreService;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \App\Services\IconStoreService\IconStoreService setDisk(string $diskName = 'icons')
+ * @method static bool usesDatabase()
+ * @method static void setDatabaseReplication(bool $usesDatabase)
+ * @method static string|null get(string $name)
+ * @method static string|false mimeType(string $name)
+ * @method static bool clear()
+ * @method static bool delete(array|string $names)
+ * @method static bool store(string $name, string $content)
+ * @method static bool exists(string $name)
+ *
  * @see \App\Services\IconStoreService
  */
 class IconStore extends Facade
--- app/Facades/Settings.php
+++ app/Facades/Settings.php
@@ -6,6 +6,12 @@ use App\Services\SettingService;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string|int|boolean|null get($setting)
+ * @method static \Illuminate\Support\Collection<string, mixed> all()
+ * @method static void set($setting, $value)
+ * @method static void delete(string $name)
+ * @method static bool isEdited($key)
+ *
  * @see \App\Services\SettingService
  */
 class Settings extends Facade
