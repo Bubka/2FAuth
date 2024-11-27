5bca296cfa6296529d7525f1171b4e94b2a2e142
Bubka <858858+Bubka@users.noreply.github.com>
2024-11-27 13:19:46 +0100

    Rebuild assets

---
+++ public/build/assets/bus-BNAtGMCo.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./app-CzxRjCVM.js";/*! 2FAuth version 5.4.3 - Copyright (c) 2024 Bubka - https://github.com/Bubka/2FAuth */const r=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
