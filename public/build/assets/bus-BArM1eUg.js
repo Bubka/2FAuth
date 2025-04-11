49d4cc8aa51537cf290a2c1ab97c405fcb1de9aa
Bubka <858858+Bubka@users.noreply.github.com>
2025-04-11 23:13:00 +0200

    Rebuild assets

--- public/build/assets/bus-BArM1eUg.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./app-DivTFvlm.js";/*! 2FAuth version 5.5.0 - Copyright (c) 2024 Bubka - https://github.com/Bubka/2FAuth */const r=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
