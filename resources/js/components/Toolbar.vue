cd9bfbc0c2369bb1cf77fccd1210bcd20172b520
Bubka <858858+Bubka@users.noreply.github.com>
2025-10-27 11:18:33 +0100

    Fix vertical alignments of toolbar items

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -9,21 +9,21 @@
 <template>
     <div class="toolbar has-text-centered">
         <div class="columns">
-            <div class="column has-nowrap px-0">
+            <div class="column has-nowrap px-0" style="line-height: initial" >
                 <!-- selected label -->
-                <span class="has-text-grey mr-1">{{ $t('message.x_selected', { count: selectedCount }) }}</span>
+                <span class="has-text-grey">{{ $t('message.x_selected', { count: selectedCount }) }}</span>
                 <!-- deselect all -->
-                <button type="button" id="btnUnselectAll" @click="$emit('clear-selected')" class="clear-selection delete mr-4" :style="{visibility: selectedCount > 0 ? 'visible' : 'hidden'}" :title="$t('tooltip.clear_selection')"></button>
+                <button type="button" id="btnUnselectAll" @click="$emit('clear-selected')" class="clear-selection delete mr-4 ml-1" :style="{visibility: selectedCount > 0 ? 'visible' : 'hidden'}" :title="$t('tooltip.clear_selection')"></button>
                 <!-- select all button -->
-                <button type="button" id="btnSelectAll" @click="$emit('select-all')" class="button mr-5 has-line-height p-1 is-ghost has-text-grey" :title="$t('tooltip.select_all')">
+                <button type="button" id="btnSelectAll" @click="$emit('select-all')" class="button py-0 px-1 mr-5 has-line-height is-ghost has-text-grey" :title="$t('tooltip.select_all')">
                     <span>{{ $t('label.check_all') }}</span>
                     <LucideSquareCheck class="ml-1" />
                 </button>
                 <!-- sort asc/desc buttons -->
-                <button type="button" id="btnSortAscending" @click="$emit('sort-asc')" class="button has-line-height p-1 is-ghost has-text-grey" :title="$t('tooltip.sort_ascending')">
+                <button type="button" id="btnSortAscending" @click="$emit('sort-asc')" class="button has-line-height p-0 is-ghost has-text-grey" :title="$t('tooltip.sort_ascending')">
                     <LucideArrowDownAZ />
                 </button>
-                <button type="button" id="btnSortDescending" @click="$emit('sort-desc')" class="button has-line-height p-1 is-ghost has-text-grey" :title="$t('tooltip.sort_descending')">
+                <button type="button" id="btnSortDescending" @click="$emit('sort-desc')" class="button has-line-height p-0 pl-2 is-ghost has-text-grey" :title="$t('tooltip.sort_descending')">
                     <LucideArrowUpAZ />
                 </button>
             </div>
