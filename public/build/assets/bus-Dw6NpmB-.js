af621d4fa76022f869990c055c249767eef0f2db
Bubka <858858+Bubka@users.noreply.github.com>
2025-11-21 17:32:26 +0100

    Rebuild assets

--- public/build/assets/bus-Dw6NpmB-.js
+++
@@ -1,1 +0,0 @@
-import{a1 as e}from"./app-C-TXXRMB.js";/*! 2FAuth version 5.6.0 - Copyright (c) 2025 Bubka - https://github.com/Bubka/2FAuth */const r=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
