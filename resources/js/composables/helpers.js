f7a8d24aad0b78b402aa980f977ea20d31d0e056
Bubka <858858+Bubka@users.noreply.github.com>
2026-04-10 23:01:11 +0200

    Add asArray helper

--- resources/js/composables/helpers.js
+++ resources/js/composables/helpers.js
@@ -2,3 +2,13 @@
 export function startsWithUppercase(str) {
     return str.substr(0, 1).match(/[A-Z\u00C0-\u00DC]/);
 }
+
+export function asArray(payload) {
+	if (Array.isArray(payload))
+		return payload
+
+	if (Array.isArray(payload?.data))
+		return payload.data
+
+	return []
+}
